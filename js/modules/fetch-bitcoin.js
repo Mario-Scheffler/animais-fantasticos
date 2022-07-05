6d287ec53ae59d7b3d1dacd308271617322faecf
Mário Scheffler <xlikkipx@gmail.com>
2022-07-05 20:21:41 -0300

    fetch bitcoin refatorado

--- js/modules/fetch-bitcoin.js
+++ js/modules/fetch-bitcoin.js
@@ -1,8 +1,8 @@
-export default function initFetchBitcoin() {
-  fetch("https://blockchain.info/ticker")
+export default function fetchBitcoin(url, target) {
+  fetch(url)
     .then((r) => r.json())
     .then((bitcoin) => {
-      const btcPreco = document.querySelector(".btc-preco");
+      const btcPreco = document.querySelector(target);
       btcPreco.innerText = (100 / bitcoin.BRL.last).toFixed(4);
     })
     .catch((erro) => {
